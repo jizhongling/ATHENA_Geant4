103042804a10588ceaeffa15957e6469e10114ea
Zhongling Ji <jizhongling@gmail.com>
2022-02-09 13:54:55 -0500

    Changed fiber cladding width

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -376,11 +376,11 @@ G4VPhysicalVolume* DetectorConstruction::DefineVolumes()
   //ECalLayerLV[1] = new G4AssemblyVolume();
 
   G4LogicalVolume* ECal_FiberCladdingLV[NumECalBlocks][NumECalBlocks];
-  G4VSolid* ECal_FiberCladdingS = new G4Tubs("ECal_FiberCladdingSolid", ECal_Fiber_r - .03*ECal_Fiber_r, ECal_Fiber_r, ECal_Thickness/2., 0.0, 360.0*deg);
+  G4VSolid* ECal_FiberCladdingS = new G4Tubs("ECal_FiberCladdingSolid", ECal_Fiber_r - .03*2.*ECal_Fiber_r, ECal_Fiber_r, ECal_Thickness/2., 0.0, 360.0*deg);
 
   // Fiber core
   G4LogicalVolume* ECal_FiberLV[NumECalBlocks][NumECalBlocks];
-  G4VSolid* ECal_FiberS = new G4Tubs("ECal_FiberSolid", 0.0, ECal_Fiber_r - .03*ECal_Fiber_r, ECal_Thickness/2., 0.0, 360.0*deg);
+  G4VSolid* ECal_FiberS = new G4Tubs("ECal_FiberSolid", 0.0, ECal_Fiber_r - .03*2.*ECal_Fiber_r, ECal_Thickness/2., 0.0, 360.0*deg);
   
   for(G4int i = 0; i < NumECalBlocks; i++)
   {
